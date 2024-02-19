17e847672c6928db1812f748905cf5604fe5f3be
Вадим <159821591+Shaitan406@users.noreply.github.com>
2024-02-19 17:57:53 +0300

    Update ZadanieVasyaTest2.java
    system.setProperty

--- home_work_java/ZadanieVasyaTest2.java
+++ home_work_java/ZadanieVasyaTest2.java
@@ -8,7 +8,7 @@ public class ZadanieVasyaTest2 {
     @Test
     public void helooVasyaTest2 ()
     {
-        System.setProperty("java.lang.System" ,"drivers\\chromedriver.exe");
+        System.setProperty("webdriver.chrome.driver" ,"drivers\chromedriver.exe");
         WebDriver driver = new ChromeDriver();
         driver.navigate().to("https://lm.skillbox.cc/qa_tester/module01/");
         driver.findElement(By.className("button")).click();
